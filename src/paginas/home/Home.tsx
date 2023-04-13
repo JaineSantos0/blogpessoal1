1f5348593e7b400a7ca40a99291bc1b7dc1edd22
Jaine Santos <jainejosiane@gmail.com>
2023-04-13 18:33:16 -0300

    Criando Home e Login

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import './Home.css'
+import Grid from '@material-ui/core/Grid/Grid'
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import './Home.css';
 
 function Home() {
     return(
         <>
-        
+            <Grid className='home'container direction="row" justifyContent="center" alignItems="center" style={{height:'100vh', width:'100vw'}}>
+                <Box>
+                    <Typography variant="h3" gutterBottom color="textPrimary" alignItems={'center'} component="h3" style={{ color: "#C45321", fontWeight: "bold" }}>Seja bem vindo(a) OTAKU!</Typography>
+                </Box>
+            </Grid>
         </>
     )
 }
